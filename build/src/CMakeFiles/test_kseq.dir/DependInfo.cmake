84d018974feac1bf33c45e601c5a29d295358621
radkevii <radkevii@gene.com>
2022-06-10 11:48:44 -0500

    replaced kseq++ with kseq.h

---
+++ build/src/CMakeFiles/test_kseq.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/radkevii/Documents/work/cumulus_barcode/cumulus_feature_barcoding/FQFeeder/src/TestKseq.cpp" "src/CMakeFiles/test_kseq.dir/TestKseq.cpp.o" "gcc" "src/CMakeFiles/test_kseq.dir/TestKseq.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/radkevii/Documents/work/cumulus_barcode/cumulus_feature_barcoding/FQFeeder/src
+# Build directory: /Users/radkevii/Documents/work/cumulus_barcode/cumulus_feature_barcoding/FQFeeder/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
